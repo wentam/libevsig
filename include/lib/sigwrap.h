3c5e90b402470fea38dd8f2fa47e7cc50106c94b
wentam <wentam42@gmail.com>
2026-04-04 19:26:56 -0400

    sigwrae fcntl

--- include/lib/sigwrap.h
+++ include/lib/sigwrap.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <stdio.h>
+#include "lib/signals.h"
 
 // TODO fmemopen
 // TODO fopencookie
@@ -24,3 +25,45 @@ int    sw_printf (const char* format, ...);
 int    sw_fprintf(FILE* stream, const char* format, ...);
 void*  sw_mmap   (void* addr, size_t len, int prot, int flags, int fd, off_t off);
 int    sw_munmap (void* addr, size_t len);
+
+// fcntl is varargs but without a vfcntl designed for forwarding these nicely.
+//
+// Instead, we'll wrap it with a macro.
+#define sw_fcntl(fd, cmd, ...) ({ \
+  int _fcntl_ret = fcntl(fd, cmd, ##__VA_ARGS__); \
+  if (_fcntl_ret == -1) { \
+    switch (errno) { \
+      case EPERM:  \
+      case EACCES: \
+        SIG_SEND(SIGNAL_PERMISSION_DENIED, "fcntl(): Permission denied", NULL, NULL); \
+        break; \
+      case EBADF: \
+        SIG_SEND(SIGNAL_BAD_FILE_DESCRIPTOR, "fcntl(): Bad file descriptor", NULL, NULL); \
+        break; \
+      case EINTR: \
+        SIG_SEND(SIGNAL_INTERRUPTED, "fcntl(): Interupted", NULL, NULL); \
+        break; \
+      case EINVAL: \
+        SIG_SEND(SIGNAL_INVALID_INPUT, "fcntl(): Invalid input", NULL, NULL); \
+        break;\
+      case EMFILE: \
+        SIG_SEND(SIGNAL_TOO_MANY_OPEN_FILES, "fcntl(): Too many open files", NULL, NULL); \
+        break; \
+      case ENOLCK: \
+        SIG_SEND(SIGNAL_TOO__MANY_LOCKS, "fcntl(): Too many locks", NULL, NULL); \
+        break; \
+      case EOVERFLOW: \
+        SIG_SEND(SIGNAL_OVERFLOW, "fcntl(): overflow", NULL, NULL); \
+        break; \
+      case ESRCH: \
+        SIG_SEND(SIGNAL_NOT_FOUND, "fcntl(): process not found", NULL, NULL); \
+        break; \
+      case EDEADLK: \
+        SIG_SEND(SIGNAL_DEADLOCK, "fcntl(): would deadlock", NULL, NULL); \
+        break; \
+      default: \
+        SIG_SEND(SIGNAL_UNKNOWN_ERROR, "fcntl(): unknown error", NULL, NULL); \
+    } \
+  } \
+  _fcntl_ret; \
+})
--- src/lib/sigwrap_socket.c
+++ src/lib/sigwrap_socket.c
@@ -8,31 +8,31 @@ int sw_socket(int domain, int type, int protocol) {
   if (out == -1) {
     switch (errno) {
       case EAFNOSUPPORT:
-        SIG_SEND(SIGNAL_UNSUPPORTED, "Address family not supported", NULL, NULL);
+        SIG_SEND(SIGNAL_UNSUPPORTED, "socket(): Address family not supported", NULL, NULL);
         break;
       case EMFILE:
-        SIG_SEND(SIGNAL_TOO_MANY_OPEN_FILES, "Too many open files", NULL, NULL);
+        SIG_SEND(SIGNAL_TOO_MANY_OPEN_FILES, "socket(): Too many open files", NULL, NULL);
         break;
       case ENFILE:
-        SIG_SEND(SIGNAL_TOO_MANY_OPEN_FILES, "Too many open files", NULL, NULL);
+        SIG_SEND(SIGNAL_TOO_MANY_OPEN_FILES, "socket(): Too many open files", NULL, NULL);
         break;
       case EPROTONOSUPPORT:
-        SIG_SEND(SIGNAL_UNSUPPORTED, "Unsupported protocol", NULL, NULL);
+        SIG_SEND(SIGNAL_UNSUPPORTED, "socket(): Unsupported protocol", NULL, NULL);
         break;
       case EPROTOTYPE:
-        SIG_SEND(SIGNAL_UNSUPPORTED, "Socket type not supported by protocol", NULL, NULL);
+        SIG_SEND(SIGNAL_UNSUPPORTED, "socket(): Socket type not supported by protocol", NULL, NULL);
         break;
       case EACCES:
-        SIG_SEND(SIGNAL_PERMISSION_DENIED, "Permission denied", NULL, NULL);
+        SIG_SEND(SIGNAL_PERMISSION_DENIED, "socket(): Permission denied", NULL, NULL);
         break;
       case ENOBUFS:
-        SIG_SEND(SIGNAL_INSUFFICIENT_RESOURCES, "Insufficient resources", NULL, NULL);
+        SIG_SEND(SIGNAL_INSUFFICIENT_RESOURCES, "socket(): Insufficient resources", NULL, NULL);
         break;
       case ENOMEM:
-        SIG_SEND(SIGNAL_NOT_ENOUGH_MEMORY, "Insufficient memory available", NULL, NULL);
+        SIG_SEND(SIGNAL_NOT_ENOUGH_MEMORY, "socket(): Insufficient memory available", NULL, NULL);
         break;
       default:
-        SIG_SEND(SIGNAL_UNKNOWN_ERROR, "socket: unknown error", NULL, NULL);
+        SIG_SEND(SIGNAL_UNKNOWN_ERROR, "socket(): unknown error", NULL, NULL);
     }
   }
 
@@ -49,33 +49,33 @@ int sw_setsockopt(int socket,
   if (out == -1) {
     switch (errno) {
       case EBADF:
-        SIG_SEND(SIGNAL_BAD_FILE_DESCRIPTOR, "Bad file descriptor", NULL, NULL);
+        SIG_SEND(SIGNAL_BAD_FILE_DESCRIPTOR, "setsockopt: Bad file descriptor", NULL, NULL);
         break;
       case EDOM:
-        SIG_SEND(SIGNAL_UNSUPPORTED, "Timeout values too large for socket structure", NULL, NULL);
+        SIG_SEND(SIGNAL_UNSUPPORTED, "setsockopt: Timeout values too large for socket structure", NULL, NULL);
         break;
       case EINVAL:
-        SIG_SEND(SIGNAL_INVALID_INPUT, "Invalid option", NULL, NULL);
+        SIG_SEND(SIGNAL_INVALID_INPUT, "setsockopt: Invalid option", NULL, NULL);
         break;
       case EISCONN:
         SIG_SEND(SIGNAL_SOCK_ALREADY_CONNECTED,
-                 "Socket already connected, but this option can't be set while connected",
+                 "setsockopt: Socket already connected, but this option can't be set while connected",
                  NULL, NULL);
         break;
       case ENOPROTOOPT:
-        SIG_SEND(SIGNAL_UNSUPPORTED, "Option not supported", NULL, NULL);
+        SIG_SEND(SIGNAL_UNSUPPORTED, "setsockopt: Option not supported", NULL, NULL);
         break;
       case ENOTSOCK:
-        SIG_SEND(SIGNAL_NOT_SOCKET, "Socket argument does not refer to socket", NULL, NULL);
+        SIG_SEND(SIGNAL_NOT_SOCKET, "setsockopt: Socket argument does not refer to socket", NULL, NULL);
         break;
       case ENOMEM:
-        SIG_SEND(SIGNAL_NOT_ENOUGH_MEMORY, "Not enough memory", NULL, NULL);
+        SIG_SEND(SIGNAL_NOT_ENOUGH_MEMORY, "setsockopt: Not enough memory", NULL, NULL);
         break;
       case ENOBUFS:
-        SIG_SEND(SIGNAL_INSUFFICIENT_RESOURCES, "Insufficient resources", NULL, NULL);
+        SIG_SEND(SIGNAL_INSUFFICIENT_RESOURCES, "setsockopt: Insufficient resources", NULL, NULL);
         break;
       default:
-        SIG_SEND(SIGNAL_UNKNOWN_ERROR, "setsockopt: unknown error", NULL, NULL);
+        SIG_SEND(SIGNAL_UNKNOWN_ERROR, "setsockopt: setsockopt: unknown error", NULL, NULL);
     }
   }
 
@@ -88,71 +88,71 @@ int sw_bind(int socket, const struct sockaddr* address, socklen_t address_len) {
   if (out == -1) {
     switch(out) {
       case EADDRINUSE:
-        SIG_SEND(SIGNAL_ADDRESS_IN_USE, "Address already in use", NULL, NULL);
+        SIG_SEND(SIGNAL_ADDRESS_IN_USE, "bind: Address already in use", NULL, NULL);
         break;
       case EADDRNOTAVAIL:
-        SIG_SEND(SIGNAL_ADDRESS_NOT_AVAILABLE, "Address not available from local machine",
+        SIG_SEND(SIGNAL_ADDRESS_NOT_AVAILABLE, "bind: Address not available from local machine",
                  NULL, NULL);
         break;
       case EAFNOSUPPORT:
-        SIG_SEND(SIGNAL_UNSUPPORTED, "Specified address not valid/supported", NULL, NULL);
+        SIG_SEND(SIGNAL_UNSUPPORTED, "bind: Specified address not valid/supported", NULL, NULL);
         break;
       case EALREADY:
-        SIG_SEND(SIGNAL_BUSY, "An assignment request is already in progress for this socket",
+        SIG_SEND(SIGNAL_BUSY, "bind: An assignment request is already in progress for this socket",
                  NULL, NULL);
         break;
       case EBADF:
-        SIG_SEND(SIGNAL_BAD_FILE_DESCRIPTOR, "Bad file descriptor", NULL, NULL);
+        SIG_SEND(SIGNAL_BAD_FILE_DESCRIPTOR, "bind: Bad file descriptor", NULL, NULL);
         break;
       case EINPROGRESS:
         SIG_SEND(SIGNAL_IN_PROGRESS,
-                 "O_NONBLOCK is set, but we can't bind immediately. Doing it asychronously.",
+                 "bind: O_NONBLOCK is set, but we can't bind immediately. Doing it asychronously.",
                  NULL, NULL);
         break;
       case EINVAL:
-        SIG_SEND(SIGNAL_UNSUPPORTED, "Invalid option, socket already bound, or socket shutdown",
+        SIG_SEND(SIGNAL_UNSUPPORTED, "bind: Invalid option, socket already bound, or socket shutdown",
                  NULL, NULL);
         break;
       case ENOBUFS:
-        SIG_SEND(SIGNAL_INSUFFICIENT_RESOURCES, "Insufficient resources", NULL, NULL);
+        SIG_SEND(SIGNAL_INSUFFICIENT_RESOURCES, "bind: Insufficient resources", NULL, NULL);
         break;
       case ENOTSOCK:
-        SIG_SEND(SIGNAL_NOT_SOCKET, "Socket argument does not refer to socket", NULL, NULL);
+        SIG_SEND(SIGNAL_NOT_SOCKET, "bind: Socket argument does not refer to socket", NULL, NULL);
         break;
       case EOPNOTSUPP:
-        SIG_SEND(SIGNAL_UNSUPPORTED, "Socket type does not support binding", NULL, NULL);
+        SIG_SEND(SIGNAL_UNSUPPORTED, "bind: Socket type does not support binding", NULL, NULL);
         break;
       case EACCES:
-        SIG_SEND(SIGNAL_PERMISSION_DENIED, "Permission denied", NULL, NULL);
+        SIG_SEND(SIGNAL_PERMISSION_DENIED, "bind: Permission denied", NULL, NULL);
         break;
       case EDESTADDRREQ:
-        SIG_SEND(SIGNAL_INVALID_INPUT, "Address is NULL", NULL, NULL);
+        SIG_SEND(SIGNAL_INVALID_INPUT, "bind: Address is NULL", NULL, NULL);
         break;
       case EISDIR:
-        SIG_SEND(SIGNAL_INVALID_INPUT, "Address is NULL", NULL, NULL);
+        SIG_SEND(SIGNAL_INVALID_INPUT, "bind: Address is NULL", NULL, NULL);
         break;
       case EIO:
-        SIG_SEND(SIGNAL_IO_ERROR, "I/O error", NULL, NULL);
+        SIG_SEND(SIGNAL_IO_ERROR, "bind: I/O error", NULL, NULL);
         break;
       case ELOOP:
-        SIG_SEND(SIGNAL_LOOP, "Infinite loop (of symlinks?) encountered resolving address",
+        SIG_SEND(SIGNAL_LOOP, "bind: Infinite loop (of symlinks?) encountered resolving address",
                  NULL, NULL);
         break;
       case ENAMETOOLONG:
-        SIG_SEND(SIGNAL_NAME_TOO_LONG, "Name too long", NULL, NULL);
+        SIG_SEND(SIGNAL_NAME_TOO_LONG, "bind: Name too long", NULL, NULL);
         break;
       case ENOENT:
-        SIG_SEND(SIGNAL_INVALID_INPUT, "Invalid path", NULL, NULL);
+        SIG_SEND(SIGNAL_INVALID_INPUT, "bind: Invalid path", NULL, NULL);
         break;
       case ENOTDIR:
-        SIG_SEND(SIGNAL_INVALID_INPUT, "Invalid path", NULL, NULL);
+        SIG_SEND(SIGNAL_INVALID_INPUT, "bind: Invalid path", NULL, NULL);
         break;
       case EROFS:
-        SIG_SEND(SIGNAL_READ_ONLY_FS, "Read-only filesystem", NULL, NULL);
+        SIG_SEND(SIGNAL_READ_ONLY_FS, "bind: Read-only filesystem", NULL, NULL);
         break;
       case EISCONN:
         SIG_SEND(SIGNAL_SOCK_ALREADY_CONNECTED,
-                 "Socket already connected",
+                 "bind: Socket already connected",
                  NULL, NULL);
         break;
       default:
@@ -169,29 +169,29 @@ int sw_listen(int socket, int backlog) {
   if (out == -1) {
     switch(errno) {
       case EBADF:
-        SIG_SEND(SIGNAL_BAD_FILE_DESCRIPTOR, "Bad file descriptor", NULL, NULL);
+        SIG_SEND(SIGNAL_BAD_FILE_DESCRIPTOR, "listen(): Bad file descriptor", NULL, NULL);
         break;
       case EDESTADDRREQ:
-        SIG_SEND(SIGNAL_UNSUPPORTED, "Socket not bound to a local address", NULL, NULL);
+        SIG_SEND(SIGNAL_UNSUPPORTED, "listen(): Socket not bound to a local address", NULL, NULL);
         break;
       case EINVAL:
-        SIG_SEND(SIGNAL_INVALID_INPUT, "Socket already connected or has been shut down",
+        SIG_SEND(SIGNAL_INVALID_INPUT, "listen(): Socket already connected or has been shut down",
                  NULL, NULL);
         break;
       case ENOTSOCK:
-        SIG_SEND(SIGNAL_NOT_SOCKET, "Socket argument does not refer to socket", NULL, NULL);
+        SIG_SEND(SIGNAL_NOT_SOCKET, "listen(): Socket argument does not refer to socket", NULL, NULL);
         break;
       case EOPNOTSUPP:
-        SIG_SEND(SIGNAL_UNSUPPORTED, "Socket protocol does not support listen()", NULL, NULL);
+        SIG_SEND(SIGNAL_UNSUPPORTED, "listen(): Socket protocol does not support listen()", NULL, NULL);
         break;
       case EACCES:
-        SIG_SEND(SIGNAL_PERMISSION_DENIED, "Permission denied", NULL, NULL);
+        SIG_SEND(SIGNAL_PERMISSION_DENIED, "listen(): Permission denied", NULL, NULL);
         break;
       case ENOBUFS:
-        SIG_SEND(SIGNAL_INSUFFICIENT_RESOURCES, "Insufficient resources", NULL, NULL);
+        SIG_SEND(SIGNAL_INSUFFICIENT_RESOURCES, "listen(): Insufficient resources", NULL, NULL);
         break;
       default:
-        SIG_SEND(SIGNAL_UNKNOWN_ERROR, "bind: unknown error", NULL, NULL);
+        SIG_SEND(SIGNAL_UNKNOWN_ERROR, "listen(): unknown error", NULL, NULL);
         break;
     }
   }
