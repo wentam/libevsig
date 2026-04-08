b4c6d6af86524d1a803c7faf45916767369dbe47
wentam <wentam42@gmail.com>
2026-04-08 02:00:17 -0400

    sigwrap_socket: implement sw_connect

--- include/lib/sigwrap_socket.h
+++ include/lib/sigwrap_socket.h
@@ -9,3 +9,4 @@ int sw_setsockopt(int socket,
                   socklen_t option_len);
 int sw_bind(int socket, const struct sockaddr* address, socklen_t address_len);
 int sw_listen(int socket, int backlog);
+int sw_connect(int socket, const struct sockaddr* address, socklen_t address_len);
--- src/lib/sigwrap_socket.c
+++ src/lib/sigwrap_socket.c
@@ -86,7 +86,7 @@ int sw_bind(int socket, const struct sockaddr* address, socklen_t address_len) {
   int out = bind(socket, address, address_len);
 
   if (out == -1) {
-    switch(out) {
+    switch(errno) {
       case EADDRINUSE:
         SIG_SEND(SIGNAL_ADDRESS_IN_USE, "bind: Address already in use", NULL, NULL);
         break;
@@ -157,6 +157,7 @@ int sw_bind(int socket, const struct sockaddr* address, socklen_t address_len) {
         break;
       default:
         SIG_SEND(SIGNAL_UNKNOWN_ERROR, "bind: unknown error", NULL, NULL);
+        break;
     }
   }
 
@@ -192,6 +193,106 @@ int sw_listen(int socket, int backlog) {
         break;
       default:
         SIG_SEND(SIGNAL_UNKNOWN_ERROR, "listen(): unknown error", NULL, NULL);
+        break;
+    }
+  }
+
+  return out;
+}
+
+
+int sw_connect(int socket, const struct sockaddr* address, socklen_t address_len) {
+  int out = connect(socket, address, address_len);
+
+  if (out == -1) {
+    switch (errno) {
+      case EADDRNOTAVAIL:
+        SIG_SEND(SIGNAL_ADDRESS_NOT_AVAILABLE, "connect(): Address not available from local machine",
+                 NULL, NULL);
+        break;
+      case EAFNOSUPPORT:
+        SIG_SEND(SIGNAL_UNSUPPORTED, "connect(): Address family not supported", NULL, NULL);
+        break;
+      case EALREADY:
+        SIG_SEND(SIGNAL_BUSY, "connect(): A connection request is already in progress for this socket",
+                 NULL, NULL);
+        break;
+      case EBADF:
+        SIG_SEND(SIGNAL_BAD_FILE_DESCRIPTOR, "connect(): Bad file descriptor", NULL, NULL);
+        break;
+      case ECONNREFUSED:
+        SIG_SEND(SIGNAL_CONNECTION_REFUSED, "connect(): Connection refused", NULL, NULL);
+        break;
+      case EINPROGRESS:
+        SIG_SEND(SIGNAL_IN_PROGRESS,
+                 "connect(): O_NONBLOCK is set, but we can't connect immediately. Doing it asychronously.",
+                 NULL, NULL);
+        break;
+      case EINTR:
+        SIG_SEND(SIGNAL_INTERRUPTED, "connect(): Connection attempt interrupted by signal. Connecting asynchronously.",
+                 NULL, NULL);
+        break;
+      case EISCONN:
+        SIG_SEND(SIGNAL_SOCK_ALREADY_CONNECTED,
+                 "connect(): Socket already connected",
+                 NULL, NULL);
+        break;
+      case ENETUNREACH:
+        SIG_SEND(SIGNAL_NETWORK_UNREACHABLE,
+                 "Network unreachable",
+                 NULL, NULL);
+        break;
+      case ENOTSOCK:
+        SIG_SEND(SIGNAL_NOT_SOCKET, "connect(): Socket argument does not refer to socket", NULL, NULL);
+        break;
+      case EPROTOTYPE:
+        SIG_SEND(SIGNAL_UNSUPPORTED, "connect(): Address is different type than socket bound to peer address", NULL, NULL);
+        break;
+      case ETIMEDOUT:
+        SIG_SEND(SIGNAL_CONNECTION_TIMED_OUT, "connect(): Connection timed out", NULL, NULL);
+        break;
+      case EIO:
+        SIG_SEND(SIGNAL_IO_ERROR, "connect(): I/O error", NULL, NULL);
+        break;
+      case ENAMETOOLONG:
+        SIG_SEND(SIGNAL_NAME_TOO_LONG, "connect(): Name too long", NULL, NULL);
+        break;
+      case ENOENT:
+        SIG_SEND(SIGNAL_INVALID_INPUT, "connect(): Invalid path", NULL, NULL);
+        break;
+      case ENOTDIR:
+        SIG_SEND(SIGNAL_INVALID_INPUT, "connect(): Invalid path", NULL, NULL);
+        break;
+      case EACCES:
+        SIG_SEND(SIGNAL_PERMISSION_DENIED, "connect(): Permission denied", NULL, NULL);
+        break;
+      case EADDRINUSE:
+        SIG_SEND(SIGNAL_ADDRESS_IN_USE, "connect(): Address already in use", NULL, NULL);
+        break;
+      case ECONNRESET:
+        SIG_SEND(SIGNAL_CONNECTION_RESET, "connect(): Connection reset", NULL, NULL);
+        break;
+      case EHOSTUNREACH:
+        SIG_SEND(SIGNAL_HOST_UNREACHABLE, "connect(): Host unreachable", NULL, NULL);
+        break;
+      case EINVAL:
+        SIG_SEND(SIGNAL_INVALID_INPUT, "connect(): Invalid option", NULL, NULL);
+        break;
+      case ELOOP:
+        SIG_SEND(SIGNAL_LOOP, "connect(): Infinite loop (of symlinks?) encountered resolving address",
+                 NULL, NULL);
+        break;
+      case ENETDOWN:
+        SIG_SEND(SIGNAL_INTERFACE_DOWN, "connect(): Network interface is down", NULL, NULL);
+        break;
+      case ENOBUFS:
+        SIG_SEND(SIGNAL_INSUFFICIENT_RESOURCES, "connect(): Insufficient resources", NULL, NULL);
+        break;
+      case EOPNOTSUPP:
+        SIG_SEND(SIGNAL_UNSUPPORTED, "connect(): Socket type does not support binding", NULL, NULL);
+        break;
+      default:
+        SIG_SEND(SIGNAL_UNKNOWN_ERROR, "connect(): unknown error", NULL, NULL);
         break;
     }
   }
